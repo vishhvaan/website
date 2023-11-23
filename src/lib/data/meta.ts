56bf4ebe6202eca6edd27a7004bd575d493be476
vishhvaan <vish0288@gmail.com>
2023-11-22 20:49:14 -0500

    add formatting and description

---
+++ src/lib/data/meta.ts
@@ -0,0 +1,16 @@
+// Base values for meta tags
+// So they can be added as suffixes on different pages
+// Via <svelte:head>
+
+export const siteBaseUrl = 'https://www.vishhvaan.com';
+
+export const keywords = [
+	'Vishhvaan',
+	'Personal',
+	'Website',
+];
+
+export const description =
+	"Personal Website";
+
+export const title = 'Vishhvaan Gopalakrishnan';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script>
     import '$lib/scss/global.scss';
-    // import '$lib/fonts/iosevka-slab.css';
+    
+    import { description, keywords, title, siteBaseUrl } from '$lib/data/meta';
 </script>
-  
+
+<svelte:head>
+	<link rel="“canonical”" href={siteBaseUrl} />
+	<meta name="keywords" content={keywords.join(', ')} />
+
+	<meta name="description" content={description} />
+	<meta property="og:description" content={description} />
+	<meta name="twitter:description" content={description} />
+
+	<title>{title}</title>
+	<meta property="og:title" content={title} />
+	<meta name="twitter:title" content={title} />
+</svelte:head>
+
 <slot />
