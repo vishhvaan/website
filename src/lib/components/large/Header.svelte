15978ffc258f690e0f04a5289acf1f2f17fd3e1b
vishhvaan <vish0288@gmail.com>
2024-01-01 11:40:04 -0600

    start adding header logo

--- src/lib/components/large/Header.svelte
+++ src/lib/components/large/Header.svelte
@@ -1,1 +1,8 @@
+<header>
+    <a href="/">
+        <div id="logo" style="background-image: url('%sveltekit.assets%/images/logo.jpg')"></div>
+    </a>
+
+</header>
+
 <h1>Vishhvaan Gopalakrishnan</h1>
