04486eaf6ea56bd25b55df978a867f970983c90d
vishhvaan <vish0288@gmail.com>
2024-03-18 00:53:59 -0400

    make changes to header and logo

--- src/lib/components/large/Header.svelte
+++ src/lib/components/large/Header.svelte
@@ -2,11 +2,6 @@
 	import Logo from '$lib/components/medium/Logo.svelte';
 </script>
 
-
-<a href="/">
-    <div id="logo" style="background-image: url('%sveltekit.assets%/images/logo.jpg')"></div>
-</a>
-
 <div class="header">
     <Logo />
 </div>
--- src/lib/components/medium/Logo.svelte
+++ src/lib/components/medium/Logo.svelte
@@ -1,1 +1,4 @@
-<h1>Vishhvaan Gopalakrishnan</h1>
+<a href="/">
+    <div id="logo" style="background-image: url('%sveltekit.assets%/images/logo.jpg')"></div>
+    <h1>Vishhvaan Gopalakrishnan</h1>
+</a>
