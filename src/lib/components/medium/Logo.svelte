d461564f38d32b0ebdf707f91ca463e4e5b399ba
vishhvaan <vish0288@gmail.com>
2024-03-18 18:06:59 -0400

    logo updates

--- src/lib/components/medium/Logo.svelte
+++ src/lib/components/medium/Logo.svelte
@@ -1,4 +1,37 @@
-<a href="/">
-    <div id="logo" style="background-image: url('%sveltekit.assets%/images/logo.jpg')"></div>
-    <h1>Vishhvaan Gopalakrishnan</h1>
+<script>
+    import logo from '$lib/assets/images/logo.jpg'
+</script>
+
+<a id="logo" href="/">
+    <div id="icon" style="background-image: url({logo})"></div>
+    <div id="title">
+        <h1>Vishhvaan Gopalakrishnan</h1>
+    </div>
 </a>
+
+
+<style lang="scss">
+    #logo {
+        float: left;
+
+        #icon {
+            display: inline-block;
+            // float: left;
+            margin-right: 20px;
+            width: 105px;
+            height: 105px;
+            border-radius: 5px;
+            background-size: 105px 105px;
+            background-repeat: no-repeat;
+            filter: grayscale(100%) contrast(150%);
+            -webkit-filter: grayscale(100%) contrast(150%);
+        }
+        #icon:hover {
+            filter: none;
+            -webkit-filter: none;
+        }
+    }
+
+</style>
+
+
